7b242cb4a579ac5bf51e5d7fac40ce2b8686a361
NaniBabuPallapu <nanibabuinnova@gmail.com>
2023-09-20 14:58:22 +0530

    changes in sql file

--- SQL/MySQL queries.sql
+++ SQL/MySQL queries.sql
@@ -459,19 +459,19 @@ example : 	SELECT  s1.student_id, s1.name
 			AND s1.course_id<>s2.course_id;  
 			
 
-SELF JOIN using INNER JOIN clause
--------------------------------------
-			SELECT  s1.student_id, s1.name  
-			FROM student s1  
-			INNER JOIN student s2  
-			ON s1.student_id=s2.student_id  
-			AND s1.course_id<>s2.course_id  
-			GROUP BY student_id;  
-			
-SELF-JOIN using LEFT JOIN clause
------------------------------------
+	i. SELF JOIN using INNER JOIN clause
+	-------------------------------------
+				SELECT  s1.student_id, s1.name  
+				FROM student s1  
+				INNER JOIN student s2  
+				ON s1.student_id=s2.student_id  
+				AND s1.course_id<>s2.course_id  
+				GROUP BY student_id;  
+				
+	ii. SELF-JOIN using LEFT JOIN clause
+	-----------------------------------
 
-			SELECT (CONCAT(s1.stud_lname, ' ', s2.stud_fname)) AS 'Monitor', s1.city  
-			FROM students s1  
-			LEFT JOIN students s2 ON s1.student_id=s2.student_id  
-			ORDER BY s1.city DESC;  
+				SELECT (CONCAT(s1.stud_lname, ' ', s2.stud_fname)) AS 'Monitor', s1.city  
+				FROM students s1  
+				LEFT JOIN students s2 ON s1.student_id=s2.student_id  
+				ORDER BY s1.city DESC;  
