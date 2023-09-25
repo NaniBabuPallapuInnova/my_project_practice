fa419379a63354df1d6da56ad8748ceefb0b81bc
NaniBabuPallapu <nanibabuinnova@gmail.com>
2023-09-26 00:03:13 +0530

    adding files to dev

---
+++ Java/com/welcome/collections/set/TreeSetExample.java
@@ -0,0 +1,28 @@
+package com.welcome.collections.set;
+
+import java.util.*;
+
+public class TreeSetExample {
+    public static void main(String[] args) {
+//        Set<String> set= new TreeSet<>(); // it won't work for    Iterator iterator=set.descendingIterator();
+
+        TreeSet<String> set= new TreeSet<>();
+        set.add("Ravi");
+        set.add("Ajay");
+        set.add("Vijay");
+        set.add("Ravi"); // can not accept duplicates
+        //Traversing elements
+        Iterator<String> itr=set.iterator();
+        while(itr.hasNext()){
+            System.out.println(itr.next());
+        }
+
+        System.out.println("\n");
+
+        System.out.println("==== DESCENDING ORDER ====");
+        Iterator iterator=set.descendingIterator();
+        while(iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+    }
+}
