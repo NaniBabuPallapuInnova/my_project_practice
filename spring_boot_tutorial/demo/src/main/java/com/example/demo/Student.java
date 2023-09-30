d2ce49cd0b09097161b86ecf6ce4a225cf48c6ea
NaniBabuPallapu <nanibabuinnova@gmail.com>
2023-09-30 12:07:56 +0530

    adding notes and reactive forms

---
+++ spring_boot_tutorial/demo/src/main/java/com/example/demo/Student.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+public class Student {
+
+    public int id;
+    public String name;
+
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+
+}
