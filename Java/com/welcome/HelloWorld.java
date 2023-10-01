d704508bd9bc0f00265fec36f14c0524324c998d
NaniBabuPallapu <nanibabuinnova@gmail.com>
2023-10-01 15:10:45 +0530

    new file added & changes in angular file

---
+++ Java/com/welcome/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.welcome;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("My Name is Nani");
+        int a = 10;
+        int b = 20;
+        int c = a+b;
+        System.out.println("Value : "+c);
+    }
+}
