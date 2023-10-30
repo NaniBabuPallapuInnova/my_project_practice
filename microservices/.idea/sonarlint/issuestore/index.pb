3bfbcb3035ea6fe7cdde9e1aaca6ae4369a30eaf
NaniBabuPallapu <nanipallapu369@gmail.com>
2023-10-31 01:33:42 +0530

    adding files

--- microservices/.idea/sonarlint/issuestore/index.pb
+++
@@ -1,3 +0,0 @@
-
-š
-jproduct-service/src/test/java/com/microservices/project/productservice/ProductServiceApplicationTests.java,8\8\889d9a9fb37978c0792005099b05d1696579fd27
--- microservices_springboot/product-service/src/main/java/com/microservices/project/productservice/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.microservices.project.productservice.repository;
-
-import com.microservices.project.productservice.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
